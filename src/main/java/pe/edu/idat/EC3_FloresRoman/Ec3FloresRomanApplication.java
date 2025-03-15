3b80a45b122244fce697234da7a4357125988389
JesusFlores0199 <a20204669@idat.pe>
2025-03-15 07:35:19 -0500

    initial commit

---
+++ src/main/java/pe/edu/idat/EC3_FloresRoman/Ec3FloresRomanApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.EC3_FloresRoman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec3FloresRomanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec3FloresRomanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/EC3_FloresRoman/Ec3FloresRomanApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.EC3_FloresRoman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec3FloresRomanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
