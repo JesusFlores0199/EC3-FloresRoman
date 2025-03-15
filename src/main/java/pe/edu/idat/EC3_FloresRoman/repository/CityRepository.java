14070f8c6813edd54b9d8b67b9f607ff296c83f3
JesusFlores0199 <a20204669@idat.pe>
2025-03-15 18:57:32 -0500

    Añadiendo repository: CityRepository

--- src/main/java/pe/edu/idat/EC3_FloresRoman/repository/CityRepository.java
+++ src/main/java/pe/edu/idat/EC3_FloresRoman/repository/CityRepository.java
@@ -1,4 +1,13 @@
 package pe.edu.idat.EC3_FloresRoman.repository;
 
-public interface CityRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.idat.EC3_FloresRoman.model.City;
+
+@Repository
+public interface CityRepository extends JpaRepository<City,Integer> {
+
+    //select * from City where Name = :Name
+    City findbyName(String Name);
+
 }
